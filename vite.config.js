3cd1354c9e602563a5401f666f60efddcc407fdd
Alexandre Patego <pategoalex@gmail.com>
2023-04-24 23:51:05 +0200

    minifying css and react file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { terser } from "rollup-plugin-terser";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react()],
+  build: {
+    cssMinify: true, // Activer la minification CSS
+  },
+  plugins: [
+    react(),
+    terser({
+      compress: {
+        drop_console: true,
+        ecma: 2015,
+        warnings: true,
+        passes: 3,
+      },
+      format: {
+        comments: false,
+      },
+    }),
+  ],
 });
